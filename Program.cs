d64e76a4bb0527e63d98f9b9d787af2a80ecef52
ErikaJD <erikajasmin159@gmail.com>
2026-06-08 12:42:34 -0600

    sincronizacion de contraseña

--- Program.cs
+++ Program.cs
@@ -14,43 +14,11 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// 2. CONEXIÓN INTELIGENTE Y BLINDADA
+// 2. CONEXIÓN DIRECTA CON LA CONTRASEÑA ACTUALIZADA DE RAILWAY
 builder.Services.AddDbContext<HeladeriaContext>(options =>
 {
-    var railwayEnvUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-    string connectionString;
-
-    if (!string.IsNullOrEmpty(railwayEnvUrl))
-    {
-        // Si la variable ya viene en formato tradicional "Host=...", la usamos directo
-        if (railwayEnvUrl.Contains("Host=") || railwayEnvUrl.Contains("Server="))
-        {
-            connectionString = railwayEnvUrl;
-        }
-        else if (railwayEnvUrl.StartsWith("postgres://"))
-        {
-            // Si viene en formato URL "postgres://...", la parseamos de forma segura
-            var uri = new Uri(railwayEnvUrl);
-            var userInfo = uri.UserInfo.Split(':');
-            var user = userInfo[0];
-            var password = userInfo[1];
-            var host = uri.Host;
-            var port = uri.Port;
-            var database = uri.AbsolutePath.TrimStart('/');
-
-            connectionString = $"Host={host};Port={port};Database={database};Username={user};Password={password};Include Error Detail=true;";
-        }
-        else
-        {
-            // Respaldo por si acaso
-            connectionString = "Host=postgres.railway.internal;Port=5432;Database=railway;Username=postgres;Password=kHtburGXECttprHpPdvkImCHliTrtFYG;Include Error Detail=true;";
-        }
-    }
-    else
-    {
-        // Respaldo local
-        connectionString = "Host=postgres.railway.internal;Port=5432;Database=railway;Username=postgres;Password=kHtburGXECttprHpPdvkImCHliTrtFYG;Include Error Detail=true;";
-    }
+    // Usamos el host interno de Railway con tu contraseña real y viva
+    var connectionString = "Host=postgres.railway.internal;Port=5432;Database=railway;Username=postgres;Password=kHtburGXECttprHpPdvkImCHliTrtFYG;Include Error Detail=true;";
 
     options.UseNpgsql(connectionString);
 });
@@ -70,7 +38,7 @@ using (var scope = app.Services.CreateScope())
     try
     {
         db.Database.Migrate();
-        Console.WriteLine("¡Conexión inteligente establecida con éxito!");
+        Console.WriteLine("¡Base de datos interna conectada con éxito con la nueva clave!");
     }
     catch (Exception ex)
     {

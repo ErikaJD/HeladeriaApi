09187e48a8766471936097349f337013b3ac8a0b
ErikaJD <erikajasmin159@gmail.com>
2026-06-08 09:29:30 -0600

    Convencion de nombres para postgres

--- Program.cs
+++ Program.cs
@@ -1,18 +1,21 @@
 using HeladeriaAPI.Data;
 using Microsoft.EntityFrameworkCore;
-
+using EFCore.NamingConventions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// CONFIGURACIÓN DE LA BASE DE DATOS MODIFICADA PARA POSTGRES EN LA NUBE
 builder.Services.AddDbContext<HeladeriaContext>(options =>
 {
-    var connectionString =
-        builder.Configuration.GetConnectionString("DefaultConnection");
+    // 1. Primero intenta leer la variable de entorno de Railway; si no existe, usa la local de appsettings
+    var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+                           ?? builder.Configuration.GetConnectionString("DefaultConnection");
 
-    options.UseNpgsql(connectionString);
+    options.UseNpgsql(connectionString)
+           .UseSnakeCaseNamingConvention(); // 2. Convierte automáticamente Mayúsculas a minúsculas_con_guion
 });
 
 var app = builder.Build();
@@ -23,7 +26,6 @@ app.UseSwaggerUI();
 // app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 // 🔥 MIGRACIÓN SEGURA

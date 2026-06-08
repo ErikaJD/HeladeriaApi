6862d1de3a59dbbe621007089c1a3d26d2bc88d7
ErikaJD <erikajasmin159@gmail.com>
2026-06-08 06:19:47 -0600

    fix json

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 using HeladeriaAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler =
+            ReferenceHandler.IgnoreCycles;
+    });
 
 builder.Services.AddEndpointsApiExplorer();
 
